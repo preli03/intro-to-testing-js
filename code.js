7aeaf5846262415a2fabd19af4165c85a1e24291
Patrick Mireles <patrickm.mireles@gmail.com>
2023-03-16 15:18:01 -0500

    add tests and ability to say 'hello' to  any input

--- code.js
+++ code.js
@@ -2,3 +2,20 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+//sayHello function
+function sayHello(input) {
+    if (input === "Jane") {
+    return "Hello, Jane!"} else
+        if ( input === "Alex" ) {
+            return "Hello, Alex!" } else
+                if ( input === "Pat" ) {
+                    return "Hello, Pat!"}
+                // #6 Implement the refactor
+    if (input === undefined ) {
+        return "Hello!"
+    }
+}
+
+
+
--- tests.js
+++ tests.js
@@ -13,3 +13,29 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+//Unit tests for sayHello function
+describe('sayHello', function() {
+    it('should be a defined function', function(){
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello!" when executed', function() {
+        expect(sayHello()).toBe("Hello!");
+    }); // #3 add a test to confirm actual vs. expected
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    }); // #4 add another small, simple test
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });// #5 one more test before refractoring
+        it('should return the string "Hello, Pat!" when executed', function() {
+            expect(sayHello("Pat")).toBe("Hello, Pat!");
+        });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+})
+
